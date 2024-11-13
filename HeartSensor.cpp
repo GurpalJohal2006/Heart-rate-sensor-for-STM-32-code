4562a2f1c0a5e57c20a31b00a258cdff44e3a1f1
GurpalJohal2006 <g9johal@uwaterloo.ca>
2024-11-13 09:29:29 -0500

    Update HeartSensor.cpp

--- HeartSensor.cpp
+++ HeartSensor.cpp
@@ -1,1 +1,154 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "max30102_for_stm32_hal.h" // Include MAX30102 library
+#include "usart.h"                  // Include UART library for serial communication
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+UART_HandleTypeDef huart1;
+max30102_t max30102;
+
+/* Function Prototypes -------------------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_I2C1_Init(void);
+
+/* USER CODE BEGIN 0 */
+void max30102_plot(uint32_t ir_sample, uint32_t red_sample) {
+    // This function is called each time new data is available
+    char buffer[50];
+    sprintf(buffer, "IR: %lu, RED: %lu\r\n", ir_sample, red_sample);
+    HAL_UART_Transmit(&huart1, (uint8_t *)buffer, strlen(buffer), HAL_MAX_DELAY);
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void) {
+    /* USER CODE BEGIN 1 */
+    HAL_Init();                      // Initialize the HAL Library
+    SystemClock_Config();             // Configure the system clock
+    MX_GPIO_Init();                   // Initialize GPIO
+    MX_USART1_UART_Init();            // Initialize UART
+    MX_I2C1_Init();                   // Initialize I2C
+    /* USER CODE END 1 */
+
+    /* USER CODE BEGIN 2 */
+    // Initialize MAX30102
+    max30102_init(&max30102, &hi2c1);
+    max30102_reset(&max30102);
+    max30102_clear_fifo(&max30102);
+
+    // Set up MAX30102 configurations
+    max30102_set_fifo_config(&max30102, max30102_smp_ave_8, 1, 7);
+    max30102_set_led_pulse_width(&max30102, max30102_spo2_16_bit);
+    max30102_set_adc_resolution(&max30102, max30102_spo2_adc_2048);
+    max30102_set_sampling_rate(&max30102, max30102_spo2_800);
+    max30102_set_led_current_1(&max30102, 6.2);
+    max30102_set_led_current_2(&max30102, 6.2);
+
+    // Set to SpO2 mode and enable interrupts
+    max30102_set_mode(&max30102, max30102_spo2);
+    max30102_set_a_full(&max30102, 1);
+    max30102_set_die_temp_en(&max30102, 1);
+    max30102_set_die_temp_rdy(&max30102, 1);
+    /* USER CODE END 2 */
+
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while (1) {
+        if (max30102_has_interrupt(&max30102)) {
+            max30102_interrupt_handler(&max30102);
+        }
+    }
+    /* USER CODE END WHILE */
+}
+
+/* System Clock Configuration */
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM = 16;
+    RCC_OscInitStruct.PLL.PLLN = 336;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+    RCC_OscInitStruct.PLL.PLLQ = 2;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+        Error_Handler();
+    }
+}
+
+/* USART1 Initialization Function */
+static void MX_USART1_UART_Init(void) {
+    huart1.Instance = USART1;
+    huart1.Init.BaudRate = 9600; // Or any desired baud rate
+    huart1.Init.WordLength = UART_WORDLENGTH_8B;
+    huart1.Init.StopBits = UART_STOPBITS_1;
+    huart1.Init.Parity = UART_PARITY_NONE;
+    huart1.Init.Mode = UART_MODE_TX_RX;
+    huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+    if (HAL_UART_Init(&huart1) != HAL_OK) {
+        Error_Handler();
+    }
+}
+
+/* I2C1 Initialization Function */
+static void MX_I2C1_Init(void) {
+    hi2c1.Instance = I2C1;
+    hi2c1.Init.ClockSpeed = 100000; // 100kHz for standard mode
+    hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+    hi2c1.Init.OwnAddress1 = 0;
+    hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+    hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+    hi2c1.Init.OwnAddress2 = 0;
+    hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+    hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+    if (HAL_I2C_Init(&hi2c1) != HAL_OK) {
+        Error_Handler();
+    }
+}
+
+/* GPIO Initialization Function */
+static void MX_GPIO_Init(void) {
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOH_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+}
+
+/* Error Handler Function */
+void Error_Handler(void) {
+    __disable_irq();
+    while (1) {
+    }
+}
 
