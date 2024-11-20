8673267102766c4e1154296aa9c0d335f593b4d0
GurpalJohal2006 <g9johal@uwaterloo.ca>
2024-11-20 17:19:00 -0500

    Added BUZZER_PIN and BUZZER_PORT definitions in main.h
    - Defined BUZZER_PIN as GPIO_PIN_5
    - Defined BUZZER_PORT as GPIOA
    - Centralized buzzer configurations for consistent usage in the code

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -73,7 +73,8 @@ void Error_Handler(void);
 #define SWO_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
-
+#define BUZZER_PIN GPIO_PIN_5
+#define BUZZER_PORT GPIOA
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
